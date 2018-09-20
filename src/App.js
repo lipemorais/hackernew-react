b8f0edf45f3d4b433acba682c3c6e754cf13f29f
Felipe Morais <fmorais@thoughtworks.com>
2018-09-20 17:16:09 -0300

    Introduce the list and arrow function to render the component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    ObjectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    ObjectID: 1,
+  },
+]
 class App extends Component {
   render() {
-    var helloWorld = "Welcome to Road to learn React";
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+      {list.map(item => {
+        return (
+          <div key={item.ObjectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+         );
+      })}
       </div>
     );
   }
