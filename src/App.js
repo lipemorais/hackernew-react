57251a0a8d37bf2641f0420eb3e0376dd5093c9e
Felipe Morais <fmorais@thoughtworks.com>
2018-09-23 11:56:19 -0300

    Introduces the component Button

--- src/App.js
+++ src/App.js
@@ -98,18 +98,37 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
+              <Button
                 onClick={() => onDismiss(item.ObjectID)}
-                type="button"
               >
                 Dismiss
-            </button>
+            </Button>
             </span>
           </div>
         )}
       </div>
     );
 
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick = {onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
   }
 }
 
