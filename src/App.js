450e52ca5dc7c618da67ada2c6117add7ef0d8df
Felipe Morais <fmorais@thoughtworks.com>
2018-09-23 13:35:13 -0300

    Updating based on search submission

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
-
-const isSearched = searchTerm => (item)  =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
 class App extends Component {
 
   constructor(props) {
@@ -23,9 +18,25 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
     }
 
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.setSearchTopStories =
+    // this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
   }
 
   setSearchTopStories(result) {
@@ -34,11 +45,7 @@ class App extends Component {
 
   componentDidMount() {
     const { searchTerm } = this.state;
-
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange(event) {
@@ -46,51 +53,55 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    const isNotId = item => item.ObjectID !== id;
+    const isNotId = item => item.objectID !== id;
     const updatedList = this.state.result.hits.filter(isNotId);
     this.setState({
-      result: Object.assign({}, this.state.result, {hits: updatedList})
+      result: { ...this.state.result, hits: updatedList }
     });
   }
 
   render() {
     const { searchTerm, result } = this.state;
 
-    if (!result) { return null; }
-
+    if(!result) { return null }
     return (
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children}) =>
-  <form>
-    {children} <input
+const Search = ({ value, onChange, onSubmit, children}) =>
+  <form onSubmit={onSubmit}>
+    <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.ObjectID} className="table-row">
+    {list.map(item =>
+      <div key={item.objectID} className="table-row">
         <span style={{width: '40%'}}>
           <a href={item.url}>{item.title}</a>
         </span>
@@ -99,7 +110,7 @@ const Table = ({ list, pattern, onDismiss }) =>
         <span style={{ width: '10%' }}>{item.points}</span>
         <span style={{ width: '10%' }}>
           <Button
-            onClick={() => onDismiss(item.ObjectID)}
+            onClick={() => onDismiss(item.objectID)}
             className="button-inline"
           >
             Dismiss
